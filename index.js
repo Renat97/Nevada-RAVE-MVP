b28e80cbbcca25eda8e6cfe8bd941d886aaad5be
Renat Norderhaug <renat97@gmail.com>
2021-01-10 11:17:09 -0800

    process.env.PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 ///>
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const path = require("path");
 const axios = require("axios");
 const faker = require("faker");
